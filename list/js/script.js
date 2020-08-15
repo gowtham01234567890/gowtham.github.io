feb0f6351d9306612dd0b574ffa4727c98bf8ea6
gowtham01234567890 <gowtham01234567890@gmail.com>
2020-08-15 09:37:43 +0530

    Style Chages

--- list/js/script.js
+++ list/js/script.js
@@ -1,119 +1,138 @@
-const student_list = document.querySelectorAll('.student-item');
-const perPage = 10;
+/* filtering data with search */
+const header = document.getElementsByClassName('header')[0];
 
-// Search bar variables
-const pageHeader = document.querySelector('.page-header');
-const div = document.createElement('div');
-div.className = 'student-search';
+const label = document.createElement('label');
+label.setAttribute('for', 'search');
+label.className = "student-search";
 
 const search = document.createElement('input');
-search.type="text";
-search.placeholder = "Search for students...";
+search.type = 'text';
+search.placeholder = "Search by name...";
 
 const submit = document.createElement('button');
-submit.textContent = 'search';
+submit.type = "submit";
 
-pageHeader.appendChild(div);
-div.appendChild(search);
-div.appendChild(submit);
+const img = document.createElement('img');
+img.src = "img/icn-search.svg";
+img.alt = "Search icon";
 
-// Search Function
+
+label.appendChild(search);
+label.appendChild(submit);
+submit.appendChild(img);
+
+header.appendChild(label);
+
 function srch(searchInput, names) {
    for(let i = 0; i < names.length; i++) {
      names[i].style.display = "none";
-     if((searchInput.value.length) >= 0 && (names[i].textContent.toLowerCase()).includes(searchInput.value.toLowerCase())) {
+     if((searchInput.value.length) >= 0 && (names[i].textContent.toLowerCase())
+     	.includes(searchInput.value.toLowerCase())) {
        names[i].style.display = "block";
      } 
    }
    
  }
- 
- submit.addEventListener('click', (event) => {
+
+submit.addEventListener('click', (event) => {
    event.preventDefault();
-   srch(search, student_list);
+   srch(search, student_item);
  });
  
- search.addEventListener('keyup', () => {
-   srch(search, student_list);
- });
+search.addEventListener('keyup', () => {
+	srch(search, student_item);
+});
 
-const showPage = (list, page) => {
-   const startIndex = (page * perPage) - perPage;
-   const endIndex = (page * perPage) - 1;
-   
-   for(let i = 0; i < list.length; i++) {
-      if(i >= startIndex && i <= endIndex) {
-         list[i].style.display = 'block';
-      } else {
-         list[i].style.display = 'none';
-      }
-   }
-};
-showPage(student_list, 1);
+let student_item = [];
+/* Data Pagination */
 
-const appendPageLinks = (list) => {
-   const page = document.getElementsByClassName('page')[0]; 
+const itemsPerPage = 9;
 
-   const div = document.createElement('div'); 
-   div.className = 'pagination';
-   page.appendChild(div);
+/* This creates shows only 9 items perPage */
+function showPage(list, page) {
+	const startIndex = (page * itemsPerPage) - itemsPerPage;
+	const endIndex = (page * itemsPerPage);
 
-   const ul = document.createElement('ul');
-   div.appendChild(ul);
+	const student_list = document.getElementsByClassName('student-list')[0];
+	student_list.innerHTML = '';
 
-   let pages = Math.ceil(list.length / perPage);
+	for(let i = 0; i < list.length; i++) {
+		if(i >= startIndex && i < endIndex) {
+			let li = document.createElement('li');
+			li.className = 'student-item cf';
 
-   // For loop to create Page Numbers
-   for(let i = 0; i < pages; i++) {
+			let div1 = document.createElement('div');
+			div1.className = 'student-details';
 
-      let li = document.createElement('li');
+			let img = document.createElement('img');
+			img.className = 'avatar';
+			img.src = `${list[i].picture.large}`;
 
-      let a = document.createElement('a');
-      a.setAttribute('href', '#');
-      a.textContent = i + 1;
+			let h3 = document.createElement('h3');
+			h3.textContent = `${list[i].name.title} ${list[i].name.first} ${list[i].name.last}`;
 
-      if(parseInt(a.textContent) === 1) {
-         a.className = 'active';
-      }
+			let span1 = document.createElement('span');
+			span1.className = 'email';
+			span1.textContent = `${list[i].email}`;
 
-      li.appendChild(a);
-      ul.appendChild(li);
+			let div2 = document.createElement('div');
+			div2.className = 'joined-details';
 
-      a.addEventListener('click', (event) => {
-         let aLinks = document.querySelectorAll('a');
-         for(let i = 0; i < aLinks.length; i++) {
-            aLinks[i].classList.remove('active');
-         }
-         let selectedLink = event.target;
-         let currentNumber = selectedLink.textContent;
-         selectedLink.classList.add('active');
-         showPage(list, parseInt(currentNumber));
-      });
-   
+			let span2 = document.createElement('span');
+			span2.className = 'date';
+			span2.textContent = `Joined ${list[i].registered.date}`;
 
-   } // End Of For Loop
+			
+			li.appendChild(div1);
+			div1.appendChild(img);
+			div1.appendChild(h3);
+			div1.appendChild(span1);
+			li.appendChild(div2);
+			div2.appendChild(span2);
+			student_list.appendChild(li);
+			// student_list.insertAdjacentHTML('beforeend', li.outerHTML);
+			student_item.push(li);
+		}
+	} 
+}
 
-}; // End Function
+showPage(data, 1);
 
-appendPageLinks(student_list)
+/* This function creates pages dynamically */
+function addPagination(list) {
+	let pages = Math.ceil(list.length/itemsPerPage);
+
+	let link_list = document.getElementsByClassName('link-list')[0];
+	link_list.innerHTML = '';
+
+	for(let i = 0; i < pages; i++) {
+		let li = document.createElement('li');
+		let button = document.createElement('button');
+		button.textContent = i + 1;
+
+		link_list.appendChild(li);
+		li.appendChild(button);
+
+		if(Number(button.textContent) === 1) {
+			button.className = 'active';
+		}
+
+		link_list.addEventListener('click', e => {
+			if(e.target.tagName === 'BUTTON') {
+			let buttons = document.querySelectorAll('button');
+			for(let i = 0; i < buttons.length; i++) {
+				buttons[i].classList.remove('active');
+			}
+			
+				let clicked = e.target;
+				let num = clicked.textContent;
+				clicked.classList.add('active');
+				showPage(list, Number(num));
+			}
+		});
+	}
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Call functions
+addPagination(data)
