97877c78e2e43e8c2aa0e4ebf063162857172194
gowtham01234567890 <gowtham01234567890@gmail.com>
2020-09-11 15:34:52 +0530

    Changes

--- interactive_form/js/script.js
+++ interactive_form/js/script.js
@@ -1,181 +1,174 @@
-/*=== When JavaScript is enabled it will not display ===*/
-const other = document.querySelector('#other-title');
+// When Page Loads, it adds Focus to the first input element
+window.onload = function() {
+    document.getElementById("name").focus();
+};
+
+// Hides the "other" option, and it displays only when javascript is disabled
+const other = document.getElementById("other-title");
 other.hidden = true;
 
-/*=== select theme section ===*/
-const select_theme = document.querySelector('option[value="select theme"]');
-select_theme.hidden = true;
-const selectElement = document.querySelector('#design');
+// Design
+const designSelectElement = document.querySelector('#design');
+const selectOptionDesign = document.querySelectorAll('#design option');
+selectOptionDesign[0].hidden = true;
 
-/*=== color theme section ===*/
-const color = document.querySelector('#shirt-colors');
-color.hidden = true;
-const colorElementOptions = document.querySelectorAll('#color option');
+// Shirt Color
+const selectShirtColor = document.querySelector('#shirt-colors');
+selectShirtColor.hidden = true;
 
-/*=== This listener hides all options and only show selected options ===*/
-selectElement.addEventListener('change', (e) => {
-	color.hidden = false;
+// Displays "color" options only when "design" option is selected
+designSelectElement.addEventListener('change', event => {
+    selectShirtColor.hidden = false;
+    const shirtColorOption = document.querySelectorAll('#color option');
 
-	for(let i = 0; i < colorElementOptions.length; i++) {
-		let selected = e.target.value;
+    for(let i = 0; i < shirtColorOption.length; i++) {
+        let selected = event.target.value;
 
-		colorElementOptions[i].style.display = 'none';
-		if(selected === 'js puns') {
-			if(i >= 0 && i < 3) {
-				colorElementOptions[0].selected = true;
-				colorElementOptions[i].style.display = '';
-			}
-		} else if(selected === 'heart js') {
-			if(i >= 3 && i < colorElementOptions.length) {
-				colorElementOptions[3].selected = true;
-				colorElementOptions[i].style.display = '';
-			}
-		}
-	}
+        shirtColorOption[i].style.display = "none";
+        if(selected === 'js puns') {
+            if(i >= 0 && i < 3) {
+                shirtColorOption[0].selected = true;
+                shirtColorOption[i].style.display = '';
+            }
+        } else if(selected == 'heart js') {
+            if(i >= 3 && i < shirtColorOption.length) {
+                shirtColorOption[3].selected = true;
+                shirtColorOption[i].style.display = '';
+            }
+        }
+    }
 });
 
-/*=== activities section ===*/
+// Checkboxes
 const activities = document.querySelector('.activities');
 const checkboxes = document.querySelectorAll('.activities input');
 
 const p = document.createElement('p');
 activities.appendChild(p);
-
 let totalCost = 0;
+activities.addEventListener('change', event => {
 
-/*=== calculates total cost and also disable other checkboxes if same day, date and time ===*/
-activities.addEventListener('change', (e) => {
-	let clicked = e.target;
-	let clickedType = clicked.getAttribute('data-day-and-time');
-	let dataCost = clicked.getAttribute('data-cost');
-	dataCost = Number(dataCost);
+    const clicked = event.target;
+    const clickedType = clicked.getAttribute('data-day-and-time');
+    let data_cost = clicked.getAttribute('data-cost');
+    data_cost = Number(data_cost);
 
-	if(clicked.checked) {
-		totalCost += dataCost;
-	} else {
-		totalCost -= dataCost;
-	}
+    if(clicked.checked) {
+        totalCost += data_cost;
+    } else {
+        totalCost -= data_cost;
+    }
+
 	p.textContent = `Total Cost: ${totalCost}`;
 
-	for(let i = 0; i < checkboxes.length; i++) {
-		let checkboxesType = checkboxes[i].getAttribute('data-day-and-time');
+    for(let i = 0; i < checkboxes.length; i++) {
+        let checkboxType = checkboxes[i].getAttribute('data-day-and-time');
 
-		if(clickedType === checkboxesType && clicked !== checkboxes[i]) {
-			if(clicked.checked) {
-				checkboxes[i].disabled = true;
-			} else {
-				checkboxes[i].disabled = false;
-			}
-		}
-	}
-});
+        if(clickedType === checkboxType && clicked !== checkboxes[i]) {
+            if(clicked.checked) {
+                checkboxes[i].disabled = true;
+            } else {
+                checkboxes[i].disabled = false;
+            }
+        }
+    }
 
-/*=== payment info ===*/
-const selectMethod = document.querySelector('#payment option[value="select method"]');
-selectMethod.hidden = true;
-const paymentMode = document.querySelector('#payment');
+}); 
 
-const credit_card = document.querySelector('#credit-card');
-// credit_card.hidden = true;
-const paypal = document.querySelector('#paypal');
+// Payment
+const selectPaymentElement = document.querySelector('#payment');
+const selectPaymentOption = document.querySelectorAll('#payment option');
+selectPaymentOption[0].hidden = true;
+selectPaymentOption[1].selected = true;
+
+const creditcard = document.getElementById('credit-card');
+creditcard.hidden = false;
+const paypal = document.getElementById('paypal');
 paypal.hidden = true;
-const bitcoin = document.querySelector('#bitcoin');
+const bitcoin = document.getElementById('bitcoin');
 bitcoin.hidden = true;
 
-paymentMode[1].selected = true;
-
-paymentMode.addEventListener('change', (e) => {
-	let selected = e.target.value;
-	
-	if(selected === 'credit card') {
-		paymentMode[1].selected = true;
-		credit_card.hidden = false;
-		paypal.hidden = true;
-		bitcoin.hidden = true;
-	} else if(selected === 'paypal') {
-		paymentMode[2].selected = true;
-		credit_card.hidden = true;
-		paypal.hidden = false;
-		bitcoin.hidden = true;
-	} else if(selected === 'bitcoin') {
-		paymentMode[3].selected = true;
-		credit_card.hidden = true;
-		paypal.hidden = true;
-		bitcoin.hidden = false;
-	}
+selectPaymentElement.addEventListener('change', event => {
+    const clicked = event.target.value;
+    console.log(clicked)
+    if(clicked === 'credit card') {
+        creditcard.hidden = false;
+        paypal.hidden = true;
+        bitcoin.hidden = true;
+    } else if(clicked === 'paypal') {
+        creditcard.hidden = true;
+        paypal.hidden = false;
+        bitcoin.hidden = true;
+    } else if(clicked === 'bitcoin') {
+        creditcard.hidden = true;
+        paypal.hidden = true;
+        bitcoin.hidden = false;
+    }
 });
 
-/*=== Validation ===*/
+// Validators
 const form = document.querySelector('form');
 const name = document.querySelector('#name');
 const email = document.querySelector('#mail');
 
-const nameValidator = () => {
-	const nameValue = name.value;
+function nameValidator() {
+    const nameValue = name.value;
+    if(nameValue.length > 0) {
+      name.style.border = '3px solid white';
+      return true;
+    } else {
+      name.style.border = '3px solid red';
+      return false;
+    }
+}
 
-	if(nameValue.length > 0) {
-		name.style.borderColor = 'white';
-		return true;
-	} else {
-		name.style.borderColor = 'red';
-		return false;
-	}
+function emailValidator() {
+    const emailValue = email.value;
+    const ind = emailValue.indexOf('@');
+    const lastInd = emailValue.lastIndexOf('.');
+    if(ind > 1 && lastInd > ind + 1) {
+        email.style.border = '3px solid white';
+        return true;
+      } else {
+        email.style.border = '3px solid red';
+        return false;
+      }
+}
 
-};
-
-const emailValidator = () => {
-	const emailValue = email.value;
-	const ind = emailValue.indexOf('@');
-	const lastInd = emailValue.lastIndexOf('.');
-
-	if(ind > 1 && lastInd > ind+1) {
-		email.style.borderColor = 'white';
-		return true;
-	} else {
-		email.style.borderColor = 'red';
-		return false;
-	}
-
-};
-
-const activitiesValidator = () => {
-	for(let i = 0; i < checkboxes.length; i++) {
-		if(checkboxes[i].checked) {
+function activitiesValidator() {
+    for(let i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked) {
 			activities.style.border = '0px solid';
 			return true;
 		}
-	}
-
-	activities.style.border = '3px solid red';
+    }
+    activities.style.border = '3px solid red';
 	return false;
 }
+  
 
-const credit = document.querySelector('#cc-num');
-const zip = document.querySelector('#zip');
-const cvv = document.querySelector('#cvv');
+const creditCardNumber = document.getElementById('cc-num');
+const zip = document.getElementById('zip');
+const cvv = document.getElementById('cvv');
 
-const  creditcardValidator = () => {
-	
-
-	if(credit_card.hidden !== true) {
-		let creditValue = credit.value;
+function creditcardValidator() {
+    if(creditcard.hidden !== true) {
+		let creditValue = creditCardNumber.value;
 		if(creditValue.length > 13 && creditValue.length < 16) {
-			credit.style.borderColor = 'white';
+			creditCardNumber.style.borderColor = 'white';
 			return true;
 		} else {
-			credit.style.borderColor = 'red';
+			creditCardNumber.style.borderColor = 'red';
 			return false;
-		}
+        }
+        
 	} else {
 		return true;
 	}
-
 };
 
-const  zipValidator = () => {
-	
-
-	if(credit_card.hidden !== true) {
+function  zipValidator() {
+	if(creditcard.hidden !== true) {
 		let zipValue = zip.value;
 		if(zipValue.length === 5) {
 			zip.style.borderColor = 'white';
@@ -190,10 +183,8 @@ const  zipValidator = () => {
 
 };
 
-const  cvvValidator = () => {
-	
-
-	if(credit_card.hidden !== true) {
+function  cvvValidator() {
+	if(creditcard.hidden !== true) {
 		let cvvValue = cvv.value;
 		if(cvvValue.length === 3) {
 			cvv.style.borderColor = 'white';
@@ -208,39 +199,45 @@ const  cvvValidator = () => {
 
 };
 
-
 form.addEventListener('submit', (e) => {
-	nameValidator();
-	emailValidator();
-	creditcardValidator();
-	zipValidator();
-	cvvValidator();
-	activitiesValidator();
+    nameValidator();
+    if(!nameValidator()) {
+        e.preventDefault();
+    }
 
-	if(!nameValidator()) {
-		e.preventDefault();
-	}
+    emailValidator();
+    if(!emailValidator()) {
+        e.preventDefault();
+    }
 
-	if(!emailValidator()) {
-		e.preventDefault();
-	}
+    activitiesValidator();
+    if(!activitiesValidator()) {
+        e.preventDefault();
+    }
 
-	if(!creditcardValidator()) {
-		e.preventDefault();
-	}
+    creditcardValidator();
+    if(!creditcardValidator()) {
+        e.preventDefault();
+    }
 
-	if(!zipValidator()) {
-		e.preventDefault();
-	}
+    zipValidator();
+    if(!zipValidator()) {
+        e.preventDefault();
+    }
 
-	if(!cvvValidator()) {
-		e.preventDefault();
-	}
-
-	if(!activitiesValidator()) {
-		e.preventDefault();
-	}
+    cvvValidator();
+    if(!cvvValidator()) {
+        e.preventDefault();
+    }
 });
+
+
+
+
+
+
+
+
 
 
 
